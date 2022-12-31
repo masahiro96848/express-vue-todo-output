0a564a69945b50b0827544bf4efe551b07ac757b
masahiro96848 <feetwcpic9436@gmail.com>
2023-01-01 00:28:23 +0900

    開発環境を修正

--- api/app.ts
+++ api/app.ts
@@ -4,6 +4,10 @@ import router from './routes/index'
 const app = express()
 const port = 3000
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
 app.use('/api', router)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
